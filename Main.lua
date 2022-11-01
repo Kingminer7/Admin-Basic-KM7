c38fcdc18e6373de8ad67298463d85858940ea63
Kingminer7 <100500023+Kingminer7@users.noreply.github.com>
2022-11-01 14:23:20 -0400

    Create Main.lua

---
+++ Main.lua
@@ -0,0 +1,13 @@
+local Info = {
+  Owner = "Admin Basic",
+  Creator = "Kingminer7",
+  Description = "An admin system, primarily used for my games.",
+  DateStarted = "November 1st, 2022",
+  DateCompleted = "Not Completed",
+]]
+-- If you want to use the commands normally, go to this page on the Roblox website: (Does not exist yet.)
+-- If you want to modify the actual code, go to this page on the Roblox website: (Does not exist yet.)
+
+-- Now here is the actual code, only edit if you know what you're doing!
+
+local serverScriptService = game:GetService("ServerScriptService")
